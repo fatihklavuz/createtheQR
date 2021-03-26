f38bc6f288071153a4d62e2363280971c89170c9
fatihklavuz <64463795+fatihklavuz@users.noreply.github.com>
2021-03-27 00:33:13 +0300

    Delete createQR.py

--- createQR.py
+++
@@ -1,9 +0,0 @@
-import qrcode
-
-
-with open('F:/Project/python/ip.txt','r') as liste:
- for a in liste: 
-     a=a.replace("\n", "")
-     qresim=qrcode.make(a)
-
-     qresim.save('F:/Project/python/'+a+'.png')
